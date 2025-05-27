25281faf62271f7845ca971bb528837671acbd90
Ivan Borisov <2622610@gmail.com>
2025-05-27 10:36:20 +0300

    metrics

--- landing/app/layout.tsx
+++ landing/app/layout.tsx
@@ -3,6 +3,7 @@ import { Inter } from 'next/font/google'
 import './globals.css'
 import Header from '@/components/Header'
 import Footer from '@/components/Footer'
+import Script from 'next/script'
 
 const inter = Inter({ subsets: ['latin'] })
 
@@ -18,7 +19,33 @@ export default function RootLayout({
 }) {
   return (
     <html lang="en">
+      <head>
+        {/* Yandex.Metrika counter */}
+        <Script id="yandex-metrika" strategy="afterInteractive">
+          {`
+            (function(m,e,t,r,i,k,a){m[i]=m[i]||function(){(m[i].a=m[i].a||[]).push(arguments)};
+            m[i].l=1*new Date();
+            for (var j = 0; j < document.scripts.length; j++) {if (document.scripts[j].src === r) { return; }}
+            k=e.createElement(t),a=e.getElementsByTagName(t)[0],k.async=1,k.src=r,a.parentNode.insertBefore(k,a)})
+            (window, document, "script", "https://mc.yandex.ru/metrika/tag.js", "ym");
+            ym(102193875, "init", {
+                clickmap:true,
+                trackLinks:true,
+                accurateTrackBounce:true,
+                webvisor:true
+            });
+          `}
+        </Script>
+        {/* /Yandex.Metrika counter */}
+      </head>
       <body className={inter.className}>
+        {/* Yandex.Metrika noscript */}
+        <noscript>
+          <div>
+            <img src="https://mc.yandex.ru/watch/102193875" style={{position:'absolute', left:'-9999px'}} alt="" />
+          </div>
+        </noscript>
+        {/* /Yandex.Metrika noscript */}
         <Header />
         <main>{children}</main>
         <Footer />
